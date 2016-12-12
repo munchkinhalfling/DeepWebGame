60a7eea98492abadd8709c736e3d34a43d2e43ba
munchkinhalfling <munchkin@rutins.com>
2016-12-12 12:05:11 -0500

    made it actually work

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-import * as microlang from './lib/microlang.js'
-var app = angular.module('deep-web-game', []);
+//import * as microlang from './lib/microlang.js'
+var app = angular.module('deepWebGame', []);
 app.controller('deep-web-ctrl', function($scope) {
-  $scope.fullWidth = window.width;
-  $scope.fullHeight = window.height;
+  $scope.fullWidth = window.innerWidth;
+  $scope.fullHeight = window.innerHeight;
 });
 class CodeArea extends HTMLElement {
   constructor() {
@@ -11,16 +11,22 @@ class CodeArea extends HTMLElement {
   connectedCallback() {
     this.setAttribute('contenteditable', '');
     this.code = this.innerHTML;
+    this.style.border = '1px solid black';
+    this.style.color = this.getAttribute('code-color');
+    this.style.fontFamily = 'monospace';
+    this.addEventListener('keyup', () => {this.code = this.innerHTML;});
   }
   evaluate() {
-    microlang.compandrun(this.code);
+    /*microlang.*/compandrun(this.code.replace(/<br>/g, '\n'));
   }
 }
 customElements.define('code-area', CodeArea);
 // end of config stuff
-var codeArea = document.getElementById('microlang-code-area');
-var hackBtn = document.getElementById('runbtn');
+window.onload = () => {
+  var codeArea = document.getElementById('microlang-code-area');
+  var hackBtn = document.getElementById('runbtn');
 
-hackBtn.addEventListener('click', () => {
-                         codeArea.evaluate();
-});
+  hackBtn.addEventListener('click', () => {
+                           codeArea.evaluate();
+  });
+}
