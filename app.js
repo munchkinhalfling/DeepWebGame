1ba465f1439e13bfa858183cf046a33962515346
munchkinhalfling <alex@rutins.com>
2016-12-12 11:20:27 -0500

    added <code-area> HTML element

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
+import * as microlang from './lib/microlang.js'
 var app = angular.module('deep-web-game', []);
 app.controller('deep-web-ctrl', function($scope) {
   $scope.fullWidth = window.width;
   $scope.fullHeight = window.height;
 });
+class CodeArea extends HTMLElement {
+  constructor() {
+    super();
+  }
+  connectedCallback() {
+    this.setAttribute('contenteditable', '');
+    this.code = this.innerHTML;
+  }
+  evaluate() {
+    microlang.compandrun(this.code);
+  }
+}
+customElements.define('code-area', CodeArea);
